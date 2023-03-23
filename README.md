1647720961d9290be5ca85a8da897c8c9070a8bc
trombone8417 <trombone8417@yahoo.com>
2023-03-23 19:38:48 +0800

    Template Context

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Django_eCommerce
+
+###### RUN SERVER
+python manage.py runserver
+![run_server.png](md-img/run_server.png)
+
+###### 參考資料:
+Udemy: Python eCommerce | Build a Django eCommerce Web Application
--- src/Django_eCommerce/views.py
+++ src/Django_eCommerce/views.py
@@ -3,7 +3,25 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, "home_page.html", {})
+    context = {
+        "title":"Hello World!",
+        "content": " Welcome to the homepage."
+    }
+    return render(request, "home_page.html", context)
+
+def about_page(request):
+    context = {
+        "title":"About Page",
+        "content": " Welcome to the about page."
+    }
+    return render(request, "home_page.html", context)
+
+def contact_page(request):
+    context = {
+        "title":"Contact",
+        "content": " Welcome to the contact page."
+    }
+    return render(request, "home_page.html", context)    
 
 def home_page_old(request):
     html_ = """
